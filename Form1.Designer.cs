cab4eebba6101f55b5f7c7312bb988e90298bc8d
behna <behna@DESKTOP-24S1AS3>
2022-02-01 20:54:06 +0330

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace bmp24_to_bmp565
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_open = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox_fileName = new System.Windows.Forms.TextBox();
+            this.button_convert = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label_w = new System.Windows.Forms.Label();
+            this.label_h = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_open
+            // 
+            this.button_open.Location = new System.Drawing.Point(505, 16);
+            this.button_open.Name = "button_open";
+            this.button_open.Size = new System.Drawing.Size(94, 29);
+            this.button_open.TabIndex = 0;
+            this.button_open.Text = "Open...";
+            this.button_open.UseVisualStyleBackColor = true;
+            this.button_open.Click += new System.EventHandler(this.button_open_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 49);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(128, 160);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox_fileName
+            // 
+            this.textBox_fileName.Location = new System.Drawing.Point(13, 16);
+            this.textBox_fileName.Name = "textBox_fileName";
+            this.textBox_fileName.Size = new System.Drawing.Size(486, 27);
+            this.textBox_fileName.TabIndex = 3;
+            // 
+            // button_convert
+            // 
+            this.button_convert.Location = new System.Drawing.Point(505, 91);
+            this.button_convert.Name = "button_convert";
+            this.button_convert.Size = new System.Drawing.Size(94, 29);
+            this.button_convert.TabIndex = 4;
+            this.button_convert.Text = "Convert";
+            this.button_convert.UseVisualStyleBackColor = true;
+            this.button_convert.Click += new System.EventHandler(this.button_convert_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(147, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "width:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(147, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "height:";
+            // 
+            // label_w
+            // 
+            this.label_w.AutoSize = true;
+            this.label_w.Location = new System.Drawing.Point(202, 49);
+            this.label_w.Name = "label_w";
+            this.label_w.Size = new System.Drawing.Size(18, 20);
+            this.label_w.TabIndex = 7;
+            this.label_w.Text = "...";
+            // 
+            // label_h
+            // 
+            this.label_h.AutoSize = true;
+            this.label_h.Location = new System.Drawing.Point(202, 69);
+            this.label_h.Name = "label_h";
+            this.label_h.Size = new System.Drawing.Size(18, 20);
+            this.label_h.TabIndex = 8;
+            this.label_h.Text = "...";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(509, 204);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(84, 20);
+            this.linkLabel1.TabIndex = 9;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "AVR64.com";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(618, 240);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label_h);
+            this.Controls.Add(this.label_w);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_convert);
+            this.Controls.Add(this.textBox_fileName);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button_open);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Bitmap 24 bit to RGB565 By AVR64.com";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_open;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox_fileName;
+        private System.Windows.Forms.Button button_convert;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label_w;
+        private System.Windows.Forms.Label label_h;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+// By Behnam Zakizadeh (AVR64.com) @ 2022-01-31 [1400/11/11]
+namespace bmp24_to_bmp565
+{
+    public partial class Form1 : Form
+    {
+        private FileStream input;
+        int bmp24_width = 0;
+        int bmp24_height = 0;
+        int bmp24_fileSize = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button_convert.Enabled = false;
+        }
+
+        private void button_open_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "Bitmap 24-bit files (*.bmp)|*.bmp|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = openFileDialog.FileName;
+
+                    textBox_fileName.Text = filePath;
+                    pictureBox1.Load(filePath);
+
+                    //textBox1.Clear();
+                    //textBox2.Clear();
+
+                    input = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+
+                    // skip first 2 byte 
+                    for (int i = 0; i < 2; i++)
+                    {
+                        int x = input.ReadByte();
+                    }
+
+                    byte[] bytes = new byte[4];
+                    // byte 2 to 5 contain file size
+                    bytes[3] = (byte)input.ReadByte();
+                    bytes[2] = (byte)input.ReadByte();
+                    bytes[1] = (byte)input.ReadByte();
+                    bytes[0] = (byte)input.ReadByte();
+
+                    if (BitConverter.IsLittleEndian)
+                        Array.Reverse(bytes);
+
+                    bmp24_fileSize = BitConverter.ToInt32(bytes, 0);
+
+                    // skip 12 byte 
+                    for (int i = 0; i < 12; i++)
+                    {
+                        int x = input.ReadByte();
+                    }
+
+                    // byte 18 to 21 contain width
+                    bytes[3] = (byte)input.ReadByte();
+                    bytes[2] = (byte)input.ReadByte();
+                    bytes[1] = (byte)input.ReadByte();
+                    bytes[0] = (byte)input.ReadByte();
+
+                    if (BitConverter.IsLittleEndian)
+                        Array.Reverse(bytes);
+
+                    bmp24_width = BitConverter.ToInt32(bytes, 0);
+
+
+                    // byte 22 to 25 contain height
+                    bytes[3] = (byte)input.ReadByte();
+                    bytes[2] = (byte)input.ReadByte();
+                    bytes[1] = (byte)input.ReadByte();
+                    bytes[0] = (byte)input.ReadByte();
+
+                    if (BitConverter.IsLittleEndian)
+                        Array.Reverse(bytes);
+
+                    bmp24_height = BitConverter.ToInt32(bytes, 0);
+
+
+                    label_w.Text = bmp24_width.ToString();
+                    label_h.Text = bmp24_height.ToString();
+
+                    button_convert.Enabled = true;
+                }
+            }
+        }
+
+        private void button_convert_Click(object sender, EventArgs e)
+        {
+            // calculate Padding for 4 byte alignment (https://en.wikipedia.org/wiki/BMP_file_format)
+            int w3 = bmp24_width * 3;
+            int res = w3 % 4;
+            int res2 = 0;
+            if (res == 3) res2 = 1;
+            if (res == 1) res2 = 3;
+
+            // skip to arrive to file contenet start byte [byte 54]
+            for (int i = 0; i < 28; i++)
+            {
+                int x = input.ReadByte();
+            }
+
+            int[,] bmp24 = new int[bmp24_width * 3, bmp24_height];
+            int[,] bmp24_reverse = new int[bmp24_width * 3, bmp24_height];
+            int[,] bmp16 = new int[bmp24_width, bmp24_height];
+
+            //read main bmp24 data to buffer
+            for (int h1 = 0; h1 < bmp24_height; h1++)
+            {
+                for (int w1 = 0; w1 < bmp24_width * 3; w1++)
+                {
+                    bmp24[w1, h1] = input.ReadByte();
+                }
+                for (int k = 0; k < res2; k++)
+                {
+                    int x = input.ReadByte();
+                }
+            }
+
+            //reverse data (because bmp24 files stored from buttom left to up)
+            for (int h1 = 0; h1 < bmp24_height; h1++)
+            {
+                for (int w1 = 0; w1 < bmp24_width * 3; w1++)
+                {
+                    bmp24_reverse[w1, bmp24_height - 1 - h1] = bmp24[w1, h1];
+                }
+            }
+
+            //now we have a matrix (bmp24_reverse) as same as bmp24 image
+
+
+
+            //savfe file Dialog
+            SaveFileDialog save = new SaveFileDialog();
+            save.FileName = "image.h";
+            save.Filter = "c heder file | *.h";
+            if (save.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter writer = new StreamWriter(save.OpenFile());
+
+                // convert bmp to rgb565 and write to file:
+
+                writer.WriteLine("#ifndef _IMAGE_H");
+                writer.WriteLine("#define _IMAGE_H");
+                writer.WriteLine("const uint16_t image1 [" + bmp24_height + "][" + bmp24_width + "] PROGMEM = { // h x w");
+                for (int h1 = 0; h1 < bmp24_height; h1++)
+                {
+                    writer.Write("{");
+                    for (int w1 = 0; w1 < bmp24_width * 3;)
+                    {
+                        int blue = (bmp24_reverse[w1, h1] >> 3);
+                        w1++;
+                        int green = (bmp24_reverse[w1, h1] >> 2);
+                        w1++;
+                        int red = (bmp24_reverse[w1, h1] >> 3);
+                        w1++;
+
+                        ushort f = (ushort)(((ushort)red << 11) | ((ushort)green << 5) | ((ushort)blue));
+
+                        if (w1 == bmp24_width * 3) // last element in each width
+                        {
+                            writer.Write("0x" + f.ToString("X4"));
+                        }
+                        else
+                        {
+                            writer.Write("0x" + f.ToString("X4") + ",");
+                        }
+                    }
+                    if(h1==bmp24_height-1) // last element in end of file
+                    {
+                        writer.WriteLine("}");
+                    }
+                    else
+                    {
+                        writer.WriteLine("},");
+                    }
+                    
+                }
+
+                writer.WriteLine("};");
+                writer.WriteLine("#endif");
+
+                writer.Dispose();
+                writer.Close();
+                MessageBox.Show("Completed!", "Completed", MessageBoxButtons.OK);
+            }
+
+
+
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            //System.Diagnostics.Process.Start("https://www.avr64.com");
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                FileName = "http://avr64.com",
+                UseShellExecute = true
+            };
+            Process.Start(psi);
+
+        }
+    }
+}
